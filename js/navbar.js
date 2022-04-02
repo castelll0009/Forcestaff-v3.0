e0b2791916765489e1b804c7b7f2fad0f70ad984
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-04-02 17:34:25 -0500

    funcionando responsivamente

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@
   
       //  open and close nav 
       $('#navbar-toggle').click(function() {
-        $('nav ul').slideToggle();
+        $('.nav-container ul').slideToggle();
       });
   
   
@@ -14,7 +14,7 @@
   
   
       // If a link has a dropdown, add sub menu toggle.
-      $('nav ul li a:not(:only-child)').click(function(e) {
+      $('.nav-container ul li a:not(:only-child)').click(function(e) {
         $(this).siblings('.navbar-dropdown').slideToggle(100);
   
         // Close dropdown when select another dropdown
@@ -32,15 +32,25 @@
 
   
   /*cambiar la apariencia de la barra superiro al mover la rueda del mouse hacia abajo*/
-  /*
-var var_header= document.querySelector(".navigation");
-window.onscroll = function() {
-  var y = window.scrollY;
-  if(y > 32){	  	  
-	var_header.style  =   "background-color :  rgb(0, 0, 0,0.7);";
+
+//var pre_stick= document.querySelector("#pre-stick");
+//var post_stick= document.querySelector("#post-stick");
+var pre_stick= document.getElementById("pre-stick");
+var post_stick= document.getElementById("post-stick");
+window.onscroll = function() {  
+  var y = window.scrollY; //console.log(window.scrollY);
+  if(y > 100){	   //700
+    console.log("superamos 64");         
+    $("#pre-stick").css("opacity", "0");
+    $("#pre-stick").css("pointer-events", "none");
+    $("#post-stick").css("opacity", "1");
+    $("#post-stick").css("pointer-events", "auto");
   }else{
-	var_header.style  =   "background-color : transparent";
+    console.log("normal");                  
+    $("#pre-stick").css("opacity", "1");
+    $("#pre-stick").css("pointer-events", "auto");
+    $("#post-stick").css("opacity", "0");      
+    $("#post-stick").css("pointer-events", "none");     
   }
     
 };
-*/
