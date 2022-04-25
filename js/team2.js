46c91464d525b06e08180a076b75edbc9c30ae44
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-04-24 23:51:23 -0500

    team force staff

--- js/team2.js
+++ js/team2.js
@@ -1,56 +1,106 @@
 var swiper = new Swiper('.product-slider', {
-  spaceBetween: 30,
-  paginationClickable: true,
-  slideToClickedSlide: true,
-  effect: 'fade',
-  // initialSlide: 2,
-  loop: false,
-  navigation: {
-      nextEl: '.next',
-      prevEl: '.prev'
-  },
-  // mousewheel: {
-  //     // invert: false
-  // },
-  on: {
-      init: function(){
-          var index = this.activeIndex;
+    spaceBetween: 30,
+    effect: 'fade',
+        //initialSlide: 3,
+    loop: false,
+    navigation: {
+        /*
+        nextEl: '.next',
+        prevEl: '.prev'
+        */
+        nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+    },
+    // mousewheel: {
+    //     // invert: false
+    // },
+    
+    on: {
+        init: function(){
+            var index = this.activeIndex;
 
-          var target = $('.product-slider__item').eq(index).data('target');
+            var target = $('.product-slider__item').eq(index).data('target');
 
-          console.log(target);
+            console.log(target);
 
-          $('.product-img__item').removeClass('active');
-          $('.product-img__item#'+ target).addClass('active');
-      }
-  }
+            $('.product-img__item').removeClass('active');
+            $('.product-img__item#'+ target).addClass('active');
+        }
+    }
+    
 
 });
 
-swiper.on('slideChange', function () {
-  var index = this.activeIndex;
+    swiper.on('slideChange', function () {
+        var index = this.activeIndex;
 
-  var target = $('.product-slider__item').eq(index).data('target');
+        var target = $('.product-slider__item').eq(index).data('target');
 
-  console.log(target);
+        console.log(target);
 
-  $('.product-img__item').removeClass('active');
-  $('.product-img__item#'+ target).addClass('active');
+        $('.product-img__item').removeClass('active');
+        $('.product-img__item#'+ target).addClass('active');
 
-  if(swiper.isEnd) {
-      $('.prev').removeClass('disabled');
-      $('.next').addClass('disabled');
-  } else {
-      $('.next').removeClass('disabled');
-  }
+        if(swiper.isEnd) {
+            $('.prev').removeClass('disabled');
+            $('.next').addClass('disabled');
+        } else {
+            $('.next').removeClass('disabled');
+        }
 
-  if(swiper.isBeginning) {
-      $('.prev').addClass('disabled');
-  } else {
-      $('.prev').removeClass('disabled');
-  }
+        if(swiper.isBeginning) {
+            $('.prev').addClass('disabled');
+        } else {
+            $('.prev').removeClass('disabled');
+        }
+    });
+
+    $(".js-fav").on("click", function() {
+        $(this).find('.heart').toggleClass("is-active");
+    });
+
+
+    /*cricle progress bar*/
+/*const swiper = new Swiper('.swiper-container', {
+	effect: 'coverflow',
+	centeredSlides: true,
+	slidesPerView: 1,
+    spaceBetween: 0,
+	loop: false,
+	speed: 600,
+	
+ 	autoplay: {
+		delay: 3000,
+ 	},
+	
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows: true,
+	},
+	
+	breakpoints: {
+		320: {
+			slidesPerView: 2,
+		},
+		560: {
+			slidesPerView: 3,
+		},
+		990: {
+			slidesPerView: 4,
+		}
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
-
-$(".js-fav").on("click", function() {
-  $(this).find('.heart').toggleClass("is-active");
-});
+*/
