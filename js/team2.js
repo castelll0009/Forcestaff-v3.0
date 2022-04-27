f3d4e064a204242460b43dd8783c902a28706daf
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-04-27 16:50:58 -0500

    porcentaje ajustable
    Pendiente reiniciar la variable cada vez quie cambie de tarjet

--- js/team2.js
+++ js/team2.js
@@ -21,7 +21,7 @@ var swiper = new Swiper('.product-slider', {
 
             var target = $('.product-slider__item').eq(index).data('target');
 
-            console.log(target);
+            console.log("target" +target);
 
             $('.product-img__item').removeClass('active');
             $('.product-img__item#'+ target).addClass('active');
@@ -59,6 +59,86 @@ var swiper = new Swiper('.product-slider', {
         $(this).find('.heart').toggleClass("is-active");
     });
 
+//CIRCLE PROGRESS
+//obtenemos el porcentaje escrito en la div
+var radio = $(".main-circle").attr("r"); //radio
+var circle = $('#bar');   //circle
+var pct = parseInt($(".product-inf__percent-txt").text());   //pct
+
+var stroke_dasharray =  ( 2 * (22 / 7) * radio ) ; //calculamos el dash array
+var stroke_dashoffset = ( 1 - pct / 100 ) * (2 * (22 / 7) * radio); //calculamos el dash offset
+console.log("radio: "+ radio  + " porcentaje recibido : " + pct +" stok das array " + stroke_dasharray);
+console.log("stroke_dashoffset "+ stroke_dashoffset);
+//circle.setAttribute("stroke-dasharray", stroke_dasharray); //insertamos el trazo en porcentaje
+
+//circle.css({ stroke_dashoffset: stroke_dashoffset});
+circle.attr("stroke_dasharray", stroke_dasharray );
+circle.attr("stroke_dashoffset", stroke_dashoffset );
+circle.css({ strokeDasharray: stroke_dasharray});
+circle.css({ strokeDashoffset: stroke_dashoffset});
+
+console.log("attr array"+ circle.attr("stroke_dasharray"));
+console.log("attr offset "+ circle.attr("stroke_dashoffset"));
+//circle.setAttribute("stroke-dashoffset", stroke_dashoffset); //insertamos el trazo en porcentaje
+//procedemos a poner el valor  del 	// percent for dashoffset
+//console.log("sdsadasd"+circle.css({ strokeDashoffset: p}));
+
+/*  
+stroke-dasharray: $circle-circumstance;
+stroke-dashoffset: ( 0.3 * $circle-circumstance);
+
+var val = parseInt($(".product-inf__percent-txt").text());  
+var circle = $('#bar');  
+console.log(progress_circle)  ;
+console.log(progress_circle)  ;
+if (isNaN(val)) {
+  //si no tiene ninguna valor el h1 por defecto0 mandamos un 100%
+ val = 100; 
+}
+else{
+  //calculamos el valor para llenar la barra circular
+  var r = progress_circle.attr('r');
+  console.log("r "+r);
+  var c = Math.PI*(r*2);    
+ 
+  if (val < 0) { val = 0;}
+  if (val > 100) { val = 100;}
+  
+  var pct = ((100-val)/100)*c;
+  console.log("pct" + pct);   
+  pct =56.54866776461628;     
+  progress_circle.css({ strokeDashoffset: pct});
+  //cambiamos el valor de el h1 %
+  
+    $('.product-inf__percent-txt').text(val);
+}
+/*
+//obtenemos el porcentaje escrito en la div
+  var val = parseInt($(".product-inf__percent-txt").text());  
+  var progress_circle = $('#bar');  
+  console.log(progress_circle)  ;
+console.log(progress_circle)  ;
+  if (isNaN(val)) {
+    //si no tiene ninguna valor el h1 por defecto0 mandamos un 100%
+   val = 100; 
+  }
+  else{
+    //calculamos el valor para llenar la barra circular
+    var r = progress_circle.attr('r');
+    console.log("r "+r);
+    var c = Math.PI*(r*2);    
+   
+    if (val < 0) { val = 0;}
+    if (val > 100) { val = 100;}
+    
+    var pct = ((100-val)/100)*c;
+    console.log("pct" + pct);   
+    pct =56.54866776461628;     
+    progress_circle.css({ strokeDashoffset: pct});
+    //cambiamos el valor de el h1 %
+    
+      $('.product-inf__percent-txt').text(val);
+  }
 
     /*cricle progress bar*/
 /*const swiper = new Swiper('.swiper-container', {
