74b2c5254fb482d6bbe50d77da4a08f08d9f1c8a
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-05-08 18:34:57 -0500

    gallery visual  part

---
+++ js/gallery.js
@@ -0,0 +1,11 @@
+$('.cont-swiper-slide-photos').toggle('show-item');
+$('.btn-back-gallery').on('click', function() {
+    $('.cont-swiper-slide-photos').toggle('show-item');
+    //alert("click");
+  });
+   // Hamburger toggle
+   $('.img-box').on('click', function() {
+    $('.cont-swiper-slide-photos').toggle('show-item');
+    //alert("click");
+  });
+  
