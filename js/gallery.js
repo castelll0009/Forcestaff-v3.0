c0e06e199b684ad33fb022022dbde5b06bf789f0
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-05-08 22:32:11 -0500

    even stop propagation

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,20 @@
+
+$('.cont-photos-gallery').on('click', function() {
+    event.stopPropagation();
+});
 $('.cont-swiper-slide-photos').toggle('show-item');
-$('.btn-back-gallery').on('click', function() {
-    $('.cont-swiper-slide-photos').toggle('show-item');
-    //alert("click");
-  });
+
+    $('.cont-swiper-slide-photos').on('click', function() {
+        $('.cont-swiper-slide-photos').toggle('show-item');
+        //alert("click");
+    });
+  
+    $('.btn-back-gallery').on('click', function() {
+        $('.cont-swiper-slide-photos').toggle('show-item');
+        event.stopPropagation();
+        //alert("click");
+    });
+
    // Hamburger toggle
    $('.img-box').on('click', function() {
     $('.cont-swiper-slide-photos').toggle('show-item');
