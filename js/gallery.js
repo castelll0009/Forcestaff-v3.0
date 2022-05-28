d34600ef2a69018e465b786ea1664910c6169fe2
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-05-27 22:20:23 -0500

    galllery

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,5 @@
 
+  // SWIPER slider  gallery
 $('.cont-photos-gallery').on('click', function() {
     event.stopPropagation();
 });
@@ -21,54 +22,40 @@ $('.cont-swiper-slide-photos').toggle('show-item');
     //alert("click");
   });
   
+  $(document).ready(function() {
+    // Swiper: Slider
+        new Swiper('.swiper-container', {
+            loop: true,
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            slidesPerView: 3,
+            paginationClickable: true,
+            spaceBetween: 20,
+            effect: 'fade',
+            navigation: {
+                /*
+                nextEl: '.next',
+                prevEl: '.prev'
+                */
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+                
+            },
+            breakpoints: {
+                1920: {
+                    slidesPerView: 3,
+                    spaceBetween: 30
+                },
+                1028: {
+                    slidesPerView: 2,
+                    spaceBetween: 30
+                },
+                480: {
+                    slidesPerView: 1,
+                    spaceBetween: 10
+                }
+            }
+            
+        });
+    });
 
-  // SWIPER slider  gallery
-
-  var swiper = new Swiper('.swiper-container-collections', {
-    grabCursor: true,
-    spaceBetween: 80,
-    freemode: true,
-    centeredSlides: true,
-    paginationClickable: true,
-    slideToClickedSlide: true,    
-    loop: false,
-    speed:1000,
-    slidesPerView: 'auto',
-    effect: 'coverflow',   
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 500,
-        modifier: 1,
-        slideShadows : false
-    },
-    pagination: {
-        el: '.swiper-pagination',    
-        dynamicBullets: false,
-        clickable: true    ,
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',              
-    },
-    on: { 
-        init: function(){        
-            var index = this.activeIndex;            
-            $('.swiper-pagination-bullet').eq(index).trigger('click'); //redirecciona haciendo click en el swiper-paginattion correspondiente POOM
-
-            //activamos la clase activa para el swiper de las caras
-            var target_circle_swiper = $('.circle-shadow').eq(index); //eq indica el indice actual de una lista de elementos , 
-            $('.circle-shadow').removeClass('active-circle');
-            target_circle_swiper.addClass('active-circle');             
-        }
-    }
-});
-
-swiper.on('slideChange', function () {   //Swipoer in movement  
-    var index = this.activeIndex;            
-    $('.swiper-pagination-bullet').eq(index).trigger('click'); //redirecciona haciendo click en el swiper-paginattion correspondiente POOM
-
-    //activamos la clase activa para el swiper de las caras
-    var target_circle_swiper = $('.circle-shadow').eq(index); //eq indica el indice actual de una lista de elementos , 
-    $('.circle-shadow').removeClass('active-circle');
-    target_circle_swiper.addClass('active-circle');  
-             
-});
