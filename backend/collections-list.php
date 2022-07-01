c1540f3c973a4de2ce160ab47c4b260036b0420b
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-06-30 21:09:56 -0500

    cambios en la galleria

---
+++ backend/collections-list.php
@@ -0,0 +1,26 @@
+<?php
+
+include("conection-db.php");
+
+  $query = "SELECT * 
+    FROM tbl_coleccion
+  ";
+  $result = mysqli_query($connection, $query);
+  // si no hay  encuentra resultados se cierra la conexion
+  if(!$result) {
+    die('Query Failed'. mysqli_error($connection));
+  }
+
+  $json = array();
+  while($row = mysqli_fetch_array($result)) {
+    $json[] = array(
+        'ruta_img_portada' => $row['ruta_img_portada'],
+        'nombre_coleccion' => $row['nombre_coleccion'],
+        'autor' => $row['autor'],
+        'descripcion' => $row['descripcion'],
+        'id' => $row['id']
+    );
+  }  
+  echo $jsonstring = json_encode($json);
+  //echo $jsonstring;  
+?>
--- js/app-request-forcestaff.js
+++ js/app-request-forcestaff.js
@@ -1,55 +1,97 @@
-$(function(){
-   console.log("jquery is  working") ;
+$(document).ready(function() {  
+    fetchProducts();//enlistamos  las cuentas
+    //addEditElements();  
+    //editElements();
+    searchShowElements();  
+    //searchShowElementsByDate();
+    //searchShowElementsByDateRanges();
+   // eliminarElementos();        
+    //ListUsuariosSelect(); 
+  });
+  
 
-   $("#search").keyup(function(){
-    let search = $("#search").val();
-    
+//FUNCIONES   
+
+    function searchShowElements(){
+        $("#search").keyup(function(){
+            let search = $("#search").val();       
+            $.ajax({
+                url: "backend/search-collection.php",
+                type: "POST",
+                data: {search},
+                success: function(response){
+                    console.log(response);
+                    if(!response.error) {
+                        let elements = JSON.parse(response);                                                      
+                        $('.ul-content-collection').html(getTemplateCardCollection(elements));                         
+                    }
+                }
+            })    
+           })
+        
+    }
+  
+   // Fetching Products
+ function fetchProducts() {
     $.ajax({
-        url: "backend/search-collection.php",
-        type: "POST",
-        data: {search},
-        success: function(response){
-            console.log(response);
-            if(!response.error) {
-                let tasks = JSON.parse(response);
-                let template = '';
-                tasks.forEach(task => {                    
-                  template += `
-                  <li class="grid-item center  card-castell ">
-                  <a href="gallery-collection.html">	
-                  
-                      <div class="div-content-card-collection">
-                          <div class="cont-img-services">	
-                              <img class="collection-img-portada" src="${task.ruta_img_portada}" >						
-                          
-                          </div>
-                          <div class="card-description ">
-                              <div class="cont-circular-img-collection "> <img src="imgs/collections/1 (1).gif" alt=""></div>
-                              <h4 class="">${task.nombre_coleccion}</h4>
-                              <div class="card-description__created-by">
-                                  <h5 >by</h5>
-                                  <a href="https://castelll0009.github.io/Forcestaff-v3.0/" class="text-card__creator">ForceStaff <i class="fa-regular fa-hand-point-left"></i></a>
-                              </div>
-                      
-                          
-                              <div class="description-collection">
-                                  <p>
-                                  This is the description of this collection, 
-                                  here you can find information such as the historical context of the collection and curiosities.
-                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem corrupti officia eius harum, assumenda laudantium aliquam doloribus possimus dolor asperiores nam nesciunt placeat cumque impedit ratione maiores tempora! Aliquam, voluptatum.
-                                  </p>
-                              </div>									
-                          </div>
-                      </div>	
-                  </a>									
-              </li>	
-                        ` 
-                });                
-                $('.col2-list-collections').html(template);
-            }
-        }
-    })    
-   })
-   console.log("exit") ;
+      url: 'backend/collections-list.php',
+      type: 'GET',
+      success: function(response) {
+      // console.log("RESPUESTA  FETCH"+ response);
+        const elements = JSON.parse(response);              
+        //let template_nuevo_producto = '';
+        //console.log(products);    
+        $('.ul-content-collection').html(getTemplateCardCollection(elements));           
+        //todos     
+      }
+    });
+  }
 
-});
+//FUNCION QUE CREA LA PLANTILLA HTML APARTIR DE LOS DATOS
+function getTemplateCardCollection(jsonElements){
+    let template= "";
+    jsonElements.forEach(element => {
+        
+        //let hrefStreaming = getHrefStreaming(product);
+        template += `
+        <li class="grid-item center  card-castell ">
+        <a href="gallery-collection.html">	
+        
+            <div class="div-content-card-collection">										
+                <div class="cont-img-services">	
+                    <img class="collection-img-portada" src="${element.ruta_img_portada}" alt="">																
+                </div>
+                <div class="card-description ">
+                    <div class="cont-3-images">
+                        <div class="content-shape-diamond">
+                            <div class="cont-circular-img-collection item1-show"> <img src="imgs/collections/placeholder-items.png" ></div>
+                        </div>
+                        <div class="content-shape-diamond">
+                            <div class="cont-circular-img-collection item2-show"> <img src="imgs/collections/placeholder-items.png" ></div>
+                        </div>
+                        <div class="content-shape-diamond diamond3">
+                            <div class="cont-circular-img-collection item3-show"> <img src="imgs/collections/placeholder-items.png" ></div>
+                        </div>	
+                                                                    
+                    </div>
+
+                    <h4 class="">${element.nombre_coleccion}</h4>
+                    <div class="card-description__created-by">
+                        <h5 >by</h5>
+                        <a href="https://timepixies.io/" class="text-card__creator">${element.autor} <i class="fa-regular fa-hand-point-left"></i></a>
+                    </div>
+            
+                
+                    <div class="description-collection">
+                        <p>
+                        ${element.descripcion}
+                        </p>
+                    </div>									
+                </div>
+            </div>	
+        </a>									
+    </li>	
+        `                                           
+    });
+    return template;
+}
