f13359b267bee7cfeac369faacb1919799391217
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-07-22 17:22:37 -0500

    recepcion de correos

--- backend/enviar_correos_clientes.php
+++ backend/enviar_correos_clientes.php
@@ -1,19 +1,36 @@
 <?php
-if(isset($_POST['send'])){
-    if(!empty($_POST['fullname']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['affair']) && !empty($_POST['message']) ){
+/*
+if(isset($_POST['fullname'])){
+
+    //if(!empty($_POST['fullname']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['affair']) && !empty($_POST['message']) ){
+        $to = "castelll0009@gmail.com";
+        $affair = "asunto";
+        $message = "mensaje enviado";
+        
         $fullname =  $_POST['fullname'];
         $email = $_POST['email'];
         $phone = $_POST['phone'];
         $affair = $_POST['affair'];
         $message  = $_POST['message'];
-        $header  = "From: noreply@example.com" . "\r\n"; 
-        $header  .= "Reply-To: noreply@example.com" . "\r\n";
-        $header .= "X-Mailer: PHP/".phpversion();
-        $mail = @mail($email, $affair, $message, $header);
-        if($mail){
-            echo "<h4> !Mail send successfuly</h4>";
-        }
-    }
+        
+        $headers = 'From: webmaster@example.com' . "\r\n" 
+        .'Reply-To: webmaster@example.com' . "\r\n" 
+        .'X-Mailer: PHP/' . phpversion();
+        mail($to, $affair, $message, $headers);    
+        echo "send";
 }
+
+$email_to = "castelll0009@gmail.com";
+$email_from = "castell";
+$email_subject = "that is the subject";
+$email_message = "mensajeeeee";
+
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+*/
 
 ?>
--- js/enviar_correos_clientes.js
+++ js/enviar_correos_clientes.js
@@ -1,6 +1,25 @@
 
-/*var correo_recepcion = "coinforcestaff.gmail.com";
-const postData = {      
+
+var correo_recepcion = "coinforcestaff.gmail.com";
+$('#id-form-contact-us').submit(e => {  
+  event.preventDefault();
+  const correo_destinatario1 = "Coinforcestaff@gmail.com";
+  const correo_destinatario2 = "castelll0009@gmail.com";
+  const fullname = $('#id-fullname').val();
+  const email =  $('#id-email').val();
+  const phone = $("#id-phone").val();
+  const affair = $("#id-affair").val();
+  const message = $("#id-message").val();
+
+  window.location.href = `mailto:${correo_destinatario1};${correo_destinatario2}?
+  &subject=${affair}&body=Name%20Customer%3A%20${fullname}%0AEmail%3A%20${email}%0APhone%3A%20${phone}%0ALink%20Whatsapp%20(Col)%3A%20%20https%3A%2F%2Fapi.whatsapp.com%2Fsend%3Fphone%3D57${phone}%0AMensaje%3A%20${message}%0A%0A%0A----------------
+  `;  
+});
+
+/*
+$('#id-form-contact-us').submit(e => {
+ 
+  const postData = {      
     //nombre_imagen_portada:  $('#id-image').val().replace(/C:\\fakepath\\/i, ''),      //enviamos el nombre del archivo sin la ruta                   
     fullname:  $('#id-fullname').val(),
     email:  $('#id-email').val(),
@@ -9,10 +28,12 @@ const postData = {
     message:$("#id-message").val(),
     header:$(correo_recepcion)
   };
-
-  console.log(postData.fullname);
-  */
-$.post("backend/enviar_correos_clientes.php",postData, (response) => {    
+  console.log("postData "+postData.fullname);
+  
+  $.post("backend/enviar_correos_clientes.php", (response) => {    
     //alert(response);
     console.log("RESPUESTA ENVIAR CORREO : "+ response);        
+  });
+  
 });
+*/
